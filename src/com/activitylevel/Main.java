1ebf67e0f8290cd62c71a8e1d7b6468f6fe81d44
lind <klpcrap@gmail.com>
2019-01-10 11:42:47 -0800

    update

--- src/com/activitylevel/Main.java
+++ src/com/activitylevel/Main.java
@@ -6,7 +6,9 @@ import java.util.List;
 import com.activitylevel.activity.ActivityLevelHigh;
 import com.activitylevel.activity.ActivityLevelLow;
 import com.activitylevel.activity.ActivityLevelMedium;
+import com.activitylevel.activity.ActivityLevelNone;
 import com.activitylevel.person.Adult;
+import com.activitylevel.person.Dead;
 import com.activitylevel.person.Kid;
 import com.activitylevel.person.Person;
 import com.activitylevel.person.Senior;
@@ -16,20 +18,25 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Person kid = new Kid("Susan", (short) 5, new ActivityLevelHigh());
-		Person teen = new Teenager("Peter", (short) 15, new ActivityLevelHigh());
-		Person adult = new Adult("Kurt", (short) 35, new ActivityLevelMedium());
-		Person senior = new Senior("Lauren", (short) 75, new ActivityLevelLow());
+		Person susan = new Kid("Susan", (short) 5, new ActivityLevelHigh());
+		Person peter = new Teenager("Peter", (short) 15, new ActivityLevelHigh());
+		Person kurt = new Adult("Kurt", (short) 35, new ActivityLevelMedium());
+		Person lauren = new Senior("Lauren", (short) 75, new ActivityLevelLow());
+		Person brian = new Dead("Brian", (short) -1, new ActivityLevelNone());
 
 		List<Person> persons = new ArrayList<>();
-		persons.add(kid);
-		persons.add(teen);
-		persons.add(adult);
-		persons.add(senior);
+		persons.add(susan);
+		persons.add(peter);
+		persons.add(kurt);
+		persons.add(lauren);
+		persons.add(brian);
 
+		// We want to avoid many conditional if's etc.
 		for (Person p : persons) {
 			short age = p.getAge();
-			if (age < 13) {
+			if (age < 0) {
+				System.out.println(p.getName() + ": I am dead");
+			} else if (age < 13) {
 				System.out.println(p.getName() + ": I am very active");
 			} else if (age < 20) {
 				System.out.println(p.getName() + ": I am very active");
---
+++ src/com/activitylevel/activity/ActivityLevelNone.java
@@ -0,0 +1,10 @@
+package com.activitylevel.activity;
+
+public class ActivityLevelNone implements ActivityLevel {
+
+	@Override
+	public String getActivityLevel() {
+		return "I am dead";
+	}
+
+}
